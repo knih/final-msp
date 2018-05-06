93d9934c5a4eb16caa411eb7880608ec042c0f0d
knih <ken@succ0.org>
2018-05-07 06:49:27 +0900

    Add comments about symantics

--- lib/lint_sym.mli
+++ lib/lint_sym.mli
@@ -1,9 +1,13 @@
+(* This signature represents the syntax and types of `lint` language,
+   whose implementations will be its semantics *)
 module type Symantics = sig
-  type 'a repr
+  type 'a repr (* representation type *)
 
   val int : int -> int repr
+  (* We use HOAS instead of Var construct *)
   val lam : ('a repr -> 'b repr) -> ('a -> 'b) repr
   val app : ('a -> 'b) repr -> 'a repr -> 'b repr
+  (* To encode recursion in the typed-final style, we introduce the fix operator *)
   val fix : (('a -> 'b) repr -> ('a -> 'b) repr) -> ('a -> 'b) repr
   val add : int repr -> int repr -> int repr
   val sub : int repr -> int repr -> int repr
@@ -11,7 +15,9 @@ module type Symantics = sig
   val div : int repr -> int repr -> int repr
   val ifz : int repr -> (unit -> 'a repr) -> (unit -> 'a repr) -> 'a repr
 
+  (* Evaluate an object term *)
   val eval : 'a repr -> 'a
+  (* Constructs for observing object terms of repr type from outside *)
   type 'a obs
   val observe : (unit -> 'a repr) -> 'a obs
 end
